f65b8dc335cb1b1f01c91f6e8c6f02028fc8f186
Kerstin-ux-prog <kerstintimmermann@hotmail.com>
2026-06-13 14:41:24 +0300

    Fix main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import os
-import smtplib
-import random
 from datetime import datetime
 import pandas
+import random
+import smtplib
 
-MY_EMAIL = os.environ.get("MY_EMAIL")
-PASSWORD = os.environ.get("PASSWORD")
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
 
-today = (datetime.now().month, datetime.now().day)
+today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
-
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
@@ -20,9 +18,11 @@ if today_tuple in birthdays_dict:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMPT("smpt.gmail.com") as connection:
+    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
         connection.starttls()
-        connection.login(user=MY_EMAIL, password=PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL,
-                            to_addrs=birthday_person["email"],
-                            msg=f"Subject: Happy Birthday!\n\n{contents}")
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
